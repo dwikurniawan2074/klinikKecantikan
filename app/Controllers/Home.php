4c220c256a3e4a9265bcbb7195b6d2ae2f612f29
dwikurniawan2074 <dwi.kurniawan2074@students.unila.ac.id>
2022-11-22 22:09:55 +0700

    penambahan myth auth serta fix undefined variable

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -18,21 +18,7 @@ class Home extends BaseController
 
     public function login()
     {
-        $AdminModel = new \App\Models\AdminModel();
-        $login = $this->request->getVar('login');
-        if($login){
-            $username = $this->request->getVar('username');
-            $password = $this->request->getVar('password');
-
-            if($username == '' or $password == ''){
-                $err = "Silahkan masukkan username dan password";
-            }
-            if($err){
-                session()->setFlashdata('error', $err);
-                return redirect()->to("pages/login");
-            }
-        }
-        return view('pages/login');
+        return view('pages/login', ['config' => config('Auth')]);
     }
 
     public function register(){
